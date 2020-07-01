0e45ba83c3925484cd1c0f90f33f941aaccd4318
Mark Murfin <mark@markmurfin.com>
2020-07-01 15:55:37 -0500

    Added user validation and default messageCount

--- lambda-endpoints/createUser.js
+++ lambda-endpoints/createUser.js
@@ -2,6 +2,16 @@
 const Responses = require('../common/API_Response');
 const Dynamo = require('../common/Dynamo');
 
+const userTemplate = () => ({
+	ID: null,
+	phoneNumber: null,
+	userId: null,
+	messageAllowance: 100,
+
+	// Return an array of properties that are null
+	invalidProperties: () => this.keys().filter(v => this[v] == null)
+});
+
 exports.handler = async event => {
     console.log('event', event);
 
@@ -12,9 +22,15 @@ exports.handler = async event => {
 
     let ID = event.pathParameters.ID;
     let tableName = 'user';
-    const user = JSON.parse(event.body);
-    user.ID = ID;
+	const body = {...JSON.parse(event.body), ID: ID};
+	const user = Object.assign(userTemplate(), body)
+	const invalidProperties = user.invalidProperties();
 
+	if (invalidProperties.length != 0)
+	{
+		console.log(`Invalid Properties for New User: ${invalidProperties} on user object: ${user}`);
+		return Responses._400({ message: 'Invalid properties for user'});
+	}
 
     const newUser = await Dynamo.write(user, tableName).catch(err => {
         console.log('error in dynamo write', err);
